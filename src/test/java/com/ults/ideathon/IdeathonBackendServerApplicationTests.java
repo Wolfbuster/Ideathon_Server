cf5804720f0b874fa34f796141e559c729c830f2
wolfbuster <rishikesh.b@ults.in>
2020-05-12 15:45:02 +0530

    Ideathon_Server_BaseProject

---
+++ src/test/java/com/ults/ideathon/IdeathonBackendServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ults.ideathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeathonBackendServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
